c3f2af92cf3f83142ef9dc9e5139b81b869ac3cf
Dyeggo Silva <dyeggo0102@live.com>
2023-05-07 01:29:39 -0300

    Initial commit

---
+++ src/main/java/com/dy/produtos/ApiRestProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.dy.produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestProdutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dy/produtos/ApiRestProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dy.produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
